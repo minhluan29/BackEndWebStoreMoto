2482bb332cde916be38da1feab06c31d8fc84d78
minhluan29 <minhluann291@gmail.com>
2022-12-30 16:30:11 +0700

    repair item..
    repair item..

--- src/services/itemService.js
+++ src/services/itemService.js
@@ -30,7 +30,7 @@ const addItem = (product) => {
             qty: data.qty,
             des: data.des,
             status: data.status,
-            soldCount: data.qty,
+            soldCount: 100,
           });
           resolve({
             errCode: 0,
